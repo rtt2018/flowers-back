ae12df259e3f13d98f279df4ea4b0cbfa8048fa2
Taras <r.taras.t.1@gmail.com>
2025-10-05 22:33:18 +0300

    upd: add login service

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,4 +1,4 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(16).required(),
@@ -9,4 +9,8 @@ export const registerUserSchema = Joi.object({
 export const loginUserSchema = Joi.object({
   email: Joi.string().email().max(128).required(),
   password: Joi.string().required(),
+});
+
+export const JWTAuth = Joi.object({
+  email: Joi.string().email().max(128).required(),
 });
